afee664a663303298a99d0a3a9a8071b82faecdb
ardy <ardysanka@gmail.com>
2021-07-14 15:09:15 +0700

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
                         
                         {{Auth::user()->name}}
                     </a>
-                   
+                    <p>
+                        Unpaid Order {{ count(Auth::user()->order) }}
+                     </p>
                     @endguest
               
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
